3cee6ab8373fbc6f738c26a295e1de0386fa46df
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-09-06 12:36:01 +0530

    fix - add individual css files to organize design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./css/css-reset.css";
-import "./css/homepage.css";
+import "./css/main-layout.css";
+import "./css/header.css";
+import "./css/navigation.css";
+import "./css/content.css";
 import { showAllProjectsInNav, showAddNewListDialogBox, loadPageWithDarkOrLightMode, showCreateNewTaskDialog } from "./js/ui/barrel.js";
 
 loadPageWithDarkOrLightMode();
