289b7df9661ca34efe672dfb141a943e1b680460
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-09-21 15:28:13 +0530

    add background to all tasks and starred tasks button to switch inbetween

--- src/index.js
+++ src/index.js
@@ -16,4 +16,25 @@ showCreateNewTaskDialog();
 showHelpPopup();
 showUserOptionsPopup();
 
+// Show all projects when clicked on all Tasks
+const navigation = document.querySelector("nav");
+const sectionButtons = navigation.querySelector(".section-buttons");
+const allTasksButton = sectionButtons.querySelector("#all-tasks");
+const starredTasksButton = sectionButtons.querySelector("#starred-tasks");
+
+allTasksButton.style.backgroundColor = "var(--secondary)";
+allTasksButton.addEventListener("click", ()=>{
+    showProjectCardInContent();
+    allTasksButton.style.backgroundColor = "var(--secondary)";
+    starredTasksButton.style.backgroundColor = "var(--background)";
+});
+
+// Show all starred tasks when clicked on starred tasks button
+starredTasksButton.addEventListener("click", ()=>{
+    // Todo: showStarredTasksCard();
+    starredTasksButton.style.backgroundColor = "var(--secondary)";
+    allTasksButton.style.backgroundColor = "var(--background)";
+});
+
+
 
