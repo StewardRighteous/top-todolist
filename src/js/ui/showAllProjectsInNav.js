5cbc21aab7e9fbbe7401a619f7bd2104863dd9cf
StewardRighteous <110706656+StewardRighteous@users.noreply.github.com.>
2024-09-27 15:41:16 +0530

    fix - go to all tasks section when project deleted in starred section

--- src/js/ui/showAllProjectsInNav.js
+++ src/js/ui/showAllProjectsInNav.js
@@ -2,7 +2,7 @@
 
 // Trash icon for deleting option
 import deleteImage from "../../img/trash-2.svg";
-import {listDetailsManager, ListObserver, ProjectObserver} from "../manager/barrel.js";
+import { listDetailsManager, ListObserver, ProjectObserver } from "../manager/barrel.js";
 
 export default function showAllProjectsInNav() {
     // getting navigation pane
@@ -11,8 +11,8 @@ export default function showAllProjectsInNav() {
     const projectList = listSection.querySelector(".projects");
 
     // removing previously existing projects incase of reload to show newly added project
-    function removeAllElementsFromProject(){
-        while(projectList.children.length != 0){
+    function removeAllElementsFromProject() {
+        while (projectList.children.length != 0) {
             projectList.firstChild.remove();
         }
     }
@@ -26,7 +26,7 @@ export default function showAllProjectsInNav() {
 
         const toggleShowProjectCheckbox = document.createElement("input");
         toggleShowProjectCheckbox.type = "checkbox";
-        if(listDetailsManager.getShowOrHideProjectCard(projectTitle)){
+        if (listDetailsManager.getShowOrHideProjectCard(projectTitle)) {
             toggleShowProjectCheckbox.checked = true;
         }
 
@@ -48,20 +48,28 @@ export default function showAllProjectsInNav() {
         }
 
         // Hiding and showing project card according to this checkbox
-        toggleShowProjectCheckbox.addEventListener("click", ()=> {
+        toggleShowProjectCheckbox.addEventListener("click", () => {
             listDetailsManager.changeShowOrHideProjectCard(projectTitle, toggleShowProjectCheckbox.checked);
             ProjectObserver.notify();
         });
 
-         // delete project after confirmation
-         deleteProjectButton.addEventListener("click", ()=>{
+        // delete project after confirmation
+        deleteProjectButton.addEventListener("click", () => {
+            // Go to all Tasks section - [while list is deleted when it is in starred tasks page]
+            const navigation = document.querySelector("nav");
+            const sectionButtons = navigation.querySelector(".section-buttons");
+            const allTasksButton = sectionButtons.querySelector("#all-tasks");
+            const starredTasksButton = sectionButtons.querySelector("#starred-tasks");
+
             let confirmDeleteProject = confirm("Do you want to delete this project ? \n (The deleted data cannot be retrieved )");
-            if(confirmDeleteProject){
-                debugger;
+            if (confirmDeleteProject) {
                 listDetailsManager.deleteProjectFromList(projectTitle);
+                debugger;
+                allTasksButton.style.backgroundColor = "var(--secondary)";
+                starredTasksButton.style.backgroundColor = "var(--background)";
                 ListObserver.notify();
             }
-         });
+        });
     }
 }
 
